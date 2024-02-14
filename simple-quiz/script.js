1c17d496e90dc1b5daf13208c12a004f0647482b
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-02-14 11:37:12 -0300

    feat:search bug

--- simple-quiz/script.js
+++ simple-quiz/script.js
@@ -40,3 +40,16 @@ function startQuiz() {
     nextButton.innerHTML = 'Next';
     showQuestion();
 }
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex]
+    let questionNo = currentQuestionIndex + 1
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerHTML = answer.text
+        button.classList.add('btn')
+        answerButton.append(button)
+    })
+}
+startQuiz()
